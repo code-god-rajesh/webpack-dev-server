6008e0470310f9fcb4f9983c03b464bb15f0bb1d
code-god-rajesh <rajeshbehalf@gmail.com>
2021-02-07 10:15:47 +0530

    initial commit

---
+++ docs/app.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import './style.scss';
+
+function App() {
+  const [state, setState] = useState('CLICK ME');
+
+  return <button onClick={() => setState('CLICKED')}>{state}</button>;
+}
+
+render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, 'docs', 'app.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'docs', 'index.html'),
+    }),
+  ],
+};
