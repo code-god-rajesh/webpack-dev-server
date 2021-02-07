4ee8d999095812ee531ec9dfe99e11a9c6a4f2c6
code-god-rajesh <rajeshbehalf@gmail.com>
2021-02-07 10:31:29 +0530

    third commit

--- docs/app.js
+++ docs/app.js
@@ -5,7 +5,12 @@ import './style.scss';
 function App() {
   const [state, setState] = useState('CLICK ME');
 
-  return <button onClick={() => setState('CLICKED')}>{state}</button>;
+  return (
+    <div>
+      <button onClick={() => setState('CLICKED')}>{state}</button>
+      <button onClick={() => setState('CLICKED')}>{state}</button>
+    </div>
+  );
 }
 
 render(<App />, document.getElementById('root'));
